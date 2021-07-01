5425d6d20159d310db0065924a935632d58cf833
Ngoc Khanh Vy Le <victoria.le1703@gmail.com>
2021-07-01 17:49:52 -0400

    changes

--- script.js
+++ script.js
@@ -1,14 +1,72 @@
-const canvas = document.getElementById("snakeGame");
+/////////////////////////////// OPTIONS /////////////////////////////////////////
+const howEl = document.getElementById("how");
+const howContent = document.getElementById("how-content");
+const close = document.getElementById("close");
+const reset = document.getElementById("reset");
+
+// reset button
+reset.addEventListener("click", () => {
+  window.location.reload();
+});
+
+howEl.onclick = function () {
+  howContent.style.display = "block";
+};
+
+close.onclick = function () {
+  howContent.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == howContent) {
+    howContent.style.display = "none";
+  }
+};
+
+/////////////////////////////// GAME /////////////////////////////////////////
+const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
 const scoreEl = document.getElementById("score");
-const reset = document.getElementById("reset");
 
 let score = 0;
 const box = 20;
 
+// snake
+let snake = [];
+
+snake[0] = {
+  x: Math.floor(Math.random() * 23 + 1) * box,
+  y: Math.floor(Math.random() * 23 + 1) * box,
+};
+
+// food
+function genFood() {
+  food_x = Math.floor(Math.random() * 24) * box;
+  food_y = Math.floor(Math.random() * 24) * box;
+
+  snake.forEach(function foodCollisonSnake(part) {
+    const collision = part.x == food_x && part.y == food_y;
+    if (collision) genFood();
+  });
+}
+
+// poison food
+function genPoison() {
+  poison_x = Math.floor(Math.random() * 24) * box;
+  poison_y = Math.floor(Math.random() * 24) * box;
+
+  snake.forEach(function foodCollisonSnake(part) {
+    const collSnake = part.x == poison_x && part.y == poison_x;
+    if (collSnake) genPosion();
+  });
+
+  const collFood = food_x == poison_x && food_y == poison_x;
+  if (collFood) genPosion();
+}
+
 // draw backfround
 function drawBackground() {
-  ctx.fillStyle = "rgb(1, 20, 31)";
+  ctx.fillStyle = "rgb(44, 63, 57)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
@@ -20,31 +78,30 @@ function drawGameOver() {
   ctx.fillText("GAME OVER", 250, 250);
 }
 
-// snake
-let snake = [];
-
-snake[0] = {
-  x: Math.floor(Math.random() * 23 + 1) * box,
-  y: Math.floor(Math.random() * 23 + 1) * box,
-};
-
+// draw snake
 function drawSnake() {
   for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "black";
     ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    ctx.strokeStyle = "rgb(44, 63, 57)";
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
   }
 }
-
-// food
-let food = {
-  x: Math.floor(Math.random() * 24) * box,
-  y: Math.floor(Math.random() * 24) * box,
-};
 
 // draw food
 function drawFood() {
   ctx.fillStyle = "red";
-  ctx.fillRect(food.x, food.y, box, box);
+  ctx.fillRect(food_x, food_y, box, box);
+  ctx.strokeStyle = "rgb(44, 63, 57)";
+  ctx.strokeRect(food_x, food_y, box, box);
+}
+
+// draw poison
+function drawPoison() {
+  ctx.fillStyle = "purple";
+  ctx.fillRect(poison_x, poison_y, box, box);
+  ctx.strokeStyle = "rgb(44, 63, 57)";
+  ctx.strokeRect(poison_x, poison_y, box, box);
 }
 
 // setting the direction of the snake
@@ -54,7 +111,6 @@ document.addEventListener("keydown", direction);
 function direction(event) {
   if (event.keyCode == 37 && m != "right") {
     m = "left";
-    console.log(snake.length);
   } else if (event.keyCode == 38 && m != "down") {
     m = "up";
   } else if (event.keyCode == 39 && m != "left") {
@@ -65,7 +121,7 @@ function direction(event) {
 }
 
 // checks if the snake eats itself
-function checkCollision(head, list) {
+function isEaten(head, list) {
   for (let i = 0; i < snake.length; i++) {
     if (head.x == list[i].x && head.y == list[i].y) {
       return true;
@@ -74,11 +130,15 @@ function checkCollision(head, list) {
   return false;
 }
 
+genFood();
+genPoison();
+
 // draw everything into the gameboard
 function draw() {
   drawBackground();
   drawSnake();
   drawFood();
+  drawPoison();
 
   // move the snake
   let headX = snake[0].x;
@@ -89,13 +149,21 @@ function draw() {
   if (m == "up") headY -= box;
   if (m == "down") headY += box;
 
+  // check if the snake eats poison food
+  if (poison_x == snake[0].x && poison_y == snake[0].y) {
+    score--;
+    genPoison();
+    scoreEl.innerHTML = score;
+
+    if (snake.length > 1) {
+      snake.pop();
+    }
+  }
+
   // check if snake eats food
-  if (food.x == snake[0].x && food.y == snake[0].y) {
+  if (food_x == snake[0].x && food_y == snake[0].y) {
     score++;
-    food = {
-      x: Math.floor(Math.random() * 24) * box,
-      y: Math.floor(Math.random() * 24) * box,
-    };
+    genFood();
     scoreEl.innerHTML = score;
   } else {
     snake.pop();
@@ -112,7 +180,7 @@ function draw() {
     headX > 24 * box ||
     headY < 0 ||
     headY > 24 * box ||
-    checkCollision(newHead, snake)
+    isEaten(newHead, snake)
   ) {
     drawGameOver();
     clearInterval(game);
@@ -121,8 +189,4 @@ function draw() {
   snake.unshift(newHead);
 }
 
-let game = setInterval(draw, 120);
-
-reset.addEventListener("click", () => {
-  window.location.reload();
-});
+let game = setInterval(draw, 100);
